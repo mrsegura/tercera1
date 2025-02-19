d9c60d3e6099b2a53fce6dcde2e101a0e6998a8c
Guillermo Segura <guillermosegura@Guillermos-MacBook-Pro.local>
2025-02-19 14:18:26 -0600

    Menu

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require_once "./parte_superior.php"?>
+
+
+
+<?php require_once "./parte_inferior.php"?>
---
+++ parte_superior.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang = "Spanish">
+
+<head>
+    <meta charset = "utf-8">
+    <meta name = "Proyecto Tercera" content = "Proyecto Tercera">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<menu>
+<div class = "menu-up">
+
+<a href = "./">    
+<img src="./img/WowHeado.png" alt="", height = "48"> 
+</a>
+
+
+
+</div>
+
+<div class = "menu-down">
+
+    <div class = "menu-izquierda">
+         <a href="">News</a>
+         <a href="">Discord Webhook</a>
+         <a href="">Database</a>
+         <a href="">Tools</a>
+         <a href="">Guides</a>
+         <a href="">Community</a>
+         <a href="">Premium</a>
+    </div>
+
+    <div class = "menu-derecha">
+        <a href="./"> 
+        <img src="./img/JoyS.png" alt="", height="48">
+        </a>
+
+        <a href="./"> 
+        <img src="./img/Email.png" alt="", height="48">
+        </a>
+
+        <a href="./"> 
+        <img src="./img/Intern.png" alt="", height="48">
+        </a>
+
+        <a href="./"> 
+        <img src="./img/Bell.png" alt="", height="48">
+        </a>
+
+    </div>
+
+</div>
+
+
+
+</menu>
