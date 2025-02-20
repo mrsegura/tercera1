f9976dda63a2268c6907c9a5eed212d17a5078aa
Guillermo Segura <guillermosegura@Guillermos-MacBook-Pro.local>
2025-02-19 19:23:18 -0600

    Accordion

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require_once "./parte_superior.php"?>
 
-
+<body>
+    <div class = '.main-accordion-container'> </div>
+</body>
 
 <?php require_once "./parte_inferior.php"?>
--- parte_inferior.php
+++ parte_inferior.php
@@ -5,3 +5,5 @@
 
 
 </html>
+
+<script src = "./script/accordion.js"></script>
